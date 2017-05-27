1ffca1f2be19ab857481469ea875426e7ea740e7
sillyDevice <kamilgrzeskiewicz@gmail.com>
2017-05-28 01:24:34 +0200

    Added MovementModulePresenter, TurretModulePresenter

---
+++ Assets/Modules/Movement/MovementModulePresenter.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+namespace Assets.Modules.Movement
+{
+    class MovementModulePresenter: MonoBehaviour
+
+    {
+        public HumanoidMovementModule MovementModule;
+        public Animator Animator;
+
+        Vector3 _baseCameraEulerAngles;
+        private void Start() {
+            Animator = GetComponent<Animator>();
+            _baseCameraEulerAngles = GameMechanics.Stores.CameraStore.CameraEulerAngles;
+        }
+
+        private void Update()
+        {
+            gameObject.transform.eulerAngles = _baseCameraEulerAngles;
+            var direction = MovementModule.UnitDirection;
+            direction = Quaternion.AngleAxis(-GameMechanics.Stores.CameraStore.CameraEulerAngles.y, Vector3.up) * direction; ;
+            Animator.SetFloat("H", direction.x);
+            Animator.SetFloat("V", direction.z);
+            Animator.SetFloat("Speed", MovementModule.MovementSpeed);
+            Animator.SetBool("IsMovingForward", MovementModule.MovementType == MovementType.Forward);
+        }
+    }
+}
---
+++ Assets/Modules/Turrets/TurretModulePresenter.cs
@@ -0,0 +1,31 @@
+﻿using Assets.Modules.Turrets;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+namespace Assets.Modules.Movement
+{
+    class TurretModulePresenter : MonoBehaviour
+
+    {
+        public TurretModule TurretModule;
+        public Animator Animator;
+
+        Vector3 _baseCameraEulerAngles;
+        private void Start() {
+            Animator = GetComponent<Animator>();
+            _baseCameraEulerAngles = GameMechanics.Stores.CameraStore.CameraEulerAngles;
+        }
+
+        private void Update()
+        {
+            gameObject.transform.eulerAngles = _baseCameraEulerAngles;
+            var direction = TurretModule.TurretDirection;
+            direction = Quaternion.AngleAxis(-GameMechanics.Stores.CameraStore.CameraEulerAngles.y, Vector3.up) * direction; ;
+            Animator.SetFloat("H", direction.x);
+            Animator.SetFloat("V", direction.z);
+        }        
+    }
+}
